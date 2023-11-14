1ab0ee8b82108b3f2034eb156802f35c97eb34a6
Dylan <changliangdylan@gmail.com>
2023-11-14 09:55:19 -0500

    Setting up basic routes and database schema

---
+++ server/environment/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
